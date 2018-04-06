74324f87dd6cbaf95d5b74d6443a72f15ff67481
542154968 <542154968@qq.com>
2018-04-06 11:35:09 +0800

    toast

---
+++ toast/toast.js
@@ -0,0 +1,53 @@
+import Toast from './toast.vue'
+
+let plugin = {};
+
+// 插件必须有一个install方法
+plugin.install = function( Vue, options = {} ){
+    const ToastController = Vue.extend( Toast );
+    // 关闭方法
+    ToastController.prototype.close = function( body ){
+        setTimeout( ()=>{
+            this.active = false;
+            setTimeout( ()=>{
+                body.removeChild( this.$el )
+                // 这个是css设置的动画时长
+            }, 1000 )
+        }, this.duration )
+    }
+    ToastController.prototype.open = function(){
+        this.active = true; 
+    }
+    ToastController.prototype.addDom = function( body ){
+        hideAll();
+        body.appendChild( this.$el );
+        function hideAll(){
+            let $toast = document.querySelectorAll('.toast'),
+                l = $toast.length;
+            for( let i = 0; i < l; i++ ){
+                $toast[i] && ( $toast[i].style.display = 'none' )
+            }
+        }
+    }
+    // 在Vue原型实现toast的DOM挂在、功能
+    // 用户可以通过在Vue实例通过this.$toast来访问
+    Vue.prototype.$toast = ( option = {} ) => {
+        let body = document.body || document.getElementsByTagName('body')[0],
+            instance = new ToastController().$mount( document.createElement('div') );
+        instance.duration = option.duration || options.duration || 4000;
+        instance.msg = typeof option === 'string' ? option : option.msg;
+        // 添加dom
+        instance.addDom( body )
+        // 打开遮罩
+        instance.open()
+        // 自动关闭遮罩
+        instance.close( body );
+        // 监听遮罩关闭的动画过程
+        // instance.$el.addEventListener( 'transitionend', function(){
+        //     // 可以打印下看到有四个动画过程 但是有时候并不精确 放弃了这种方法
+            
+        // } )
+    } 
+}
+
+export default plugin
---
+++ toast/toast.vue
@@ -0,0 +1,37 @@
+<template>
+    <transition name="hide" >
+        <div class="toast" v-show="active" >
+            {{msg}}
+        </div>
+    </transition>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            active: false,
+            msg: ''
+        }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+    .toast
+        position fixed
+        padding 10px 20px
+        background rgba(0, 0, 0, 0.7)
+        color #fff
+        border-radius 4px
+        margin 0 auto
+        left: 50%
+        top 20px
+        transition all .3s
+        transform translate(-50%, 0px)
+        opacity 1
+    .hide-enter, .hide-leave-to 
+        transform: translate(-50%, -100px)
+        opacity 0
+        
+</style>
