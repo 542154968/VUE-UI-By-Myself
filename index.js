172aa86853628a789cd6ad024a3d20faab2ef364
二狗蛋丶 <542154968@qq.com>
2018-08-16 18:00:28 +0800

    plugin的写法

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 import Msg from './index.vue'
 import Vue from 'vue'
-// const plugin = {}
 
-// plugin.install = function (Vue, options) {
-//   let Parent = Vue.extend(Msg)
-//   const instance = new Parent().$mount(document.createElement('div'))
-//   document.body.appendChild(instance.$el)
-// }
+/*plugin的写法*/
+
+// const plugin = {}
+// plugin.install = function(Vue, options = {}) {
+//     // 获取组件内容
+//     const $msg = Vue.extend(Msg);
+//     $msg.prototype._remove = function(el) {
+//         document.body.removeChild(el);
+//     };
+//     Vue.prototype.$Message = (options = {}) => {
+//         const instance = new $msg().$mount(document.createElement("div"));
+//         // 传入参数 props
+//         instance.type = options.type;
+//         instance.cancel = function() {
+//             typeof options.cancel === "funtion" && options.cancel();
+//             instance._remove(instance.$el);
+//         };
+//         instance.confirm = function() {
+//             typeof options.confirm === "funtion" && options.confirm();
+//             instance._remove(instance.$el);
+//         };
+//         // 添加到dom中
+//         document.body.appendChild(instance.$el);
+//     };
+//     // 实例化并挂载
+// };
 
 let Message = function (options) {
   // 获取组件内容
@@ -30,7 +50,7 @@ let Message = function (options) {
   // 添加到dom中
   document.body.appendChild(instance.$el)
 }
-// 加上这个可以直接在vue全局调用 
+// 加上这个可以直接在vue全局调用  感觉这还不是最好的方式  如何 vue.use()也可以用  直接Message()也可以用呢
 Vue.prototype.$Message = Message;
 
 export default Message
